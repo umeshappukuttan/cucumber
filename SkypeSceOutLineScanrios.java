8ba2dae2c4f84b581ee85af0200686cf4ffca306
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2019-08-15 08:47:30 +0530

    skypescenarios-feature-java-files

---
+++ SkypeSceOutLineScanrios.java
@@ -0,0 +1,14 @@
+package cucumber.features;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import cucumber.api.java.en.Given;
+
+public class SkypeSceOutLineScanrios {
+		
+}
